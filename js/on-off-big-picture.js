91113c68194a91c9a094638f3de66811c3d36225
OlegSulitskii <sulitsky2011@yandex.ru>
2025-02-18 01:43:52 +0400

    ДЗ - 8.15. Открывается и закрывается (часть 2)

--- js/on-off-big-picture.js
+++ js/on-off-big-picture.js
@@ -1,25 +1,24 @@
-import { renderComments } from './render-comments';
+import { renderComments, clearOldComments } from './render-comments';
 
 const bigPictureElement = document.querySelector('.big-picture');
 const picturesElement = document.querySelector('.pictures');
 const bigPictureCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
-const socialCommentCountElement = bigPictureElement.querySelector('.social__comment-count');
-const commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
 const bodyElement = document.querySelector('body');
 
 
 const openBigPicture = () => {
+  clearOldComments();
   bigPictureElement.classList.remove('hidden');
-  socialCommentCountElement.classList.add('hidden');
-  commentsLoaderElement.classList.add('hidden');
   bodyElement.classList.add('modal-open');
 };
 
-const closeBigPicture = () => {
-  const bigPictureCloseActions = () => {
-    bigPictureElement.classList.add('hidden');
-    bodyElement.classList.remove('modal-open');
-  };
+const bigPictureCloseActions = () => {
+  bigPictureElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+};
+
+const registerEventsCloseBigPicture = () => {
+  bigPictureCloseActions();
 
   bigPictureCloseElement.addEventListener('click', () => {
     bigPictureCloseActions();
@@ -33,17 +32,13 @@ const closeBigPicture = () => {
   });
 };
 
-const pushDataBigPicture = ({photo, likes, comments, description}) => {
+const pushDataBigPicture = ({photo, likes, description}) => {
   const bigPictureImgElement = bigPictureElement.querySelector('img');
   const likesCountElement = bigPictureElement.querySelector('.likes-count');
-  const commentsTotalCountElement = bigPictureElement.querySelector('.social__comment-total-count');
-  const commentsShownCountElement = bigPictureElement.querySelector('.social__comment-shown-count');
   const altCaptionElement = bigPictureElement.querySelector('.social__caption');
 
   bigPictureImgElement.src = photo;
   likesCountElement.textContent = likes;
-  commentsTotalCountElement.textContent = comments.length;
-  commentsShownCountElement.textContent = commentsTotalCountElement.textContent;
   bigPictureImgElement.alt = description;
   altCaptionElement.textContent = bigPictureImgElement.alt;
 };
@@ -51,6 +46,7 @@ const pushDataBigPicture = ({photo, likes, comments, description}) => {
 const onOffBigPicture = (data) => {
   picturesElement.addEventListener('click', (evt) => {
     evt.preventDefault();
+
     if(evt.target.closest('.picture')) {
       openBigPicture();
 
@@ -65,7 +61,7 @@ const onOffBigPicture = (data) => {
     }
   });
 
-  closeBigPicture();
+  registerEventsCloseBigPicture();
 };
 
 export {
